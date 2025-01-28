6d8155d59cc0a268d5aaa586313ddc455517b48d
lazyBot1234 <dilishad170@gmail.com>
2025-01-28 21:58:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nqYxCKzC#bgd0edlkLIERXx2CYeC3g6wWMMdVqCA3VUtf-QqOcF8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3rIHRQ4Y#9ytuD3bgs3VouvtUEIkbraw0-V33c_H4UJb-cbGUn1Q' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
